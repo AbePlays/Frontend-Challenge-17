e1570d0e59f2cff76b63969c2f597df1d61c1465
Abe <abhi.rawat456@gmail.com>
2021-02-14 17:16:23 +0530

    Project Setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import "./tailwind.css";
+
+createApp(App).mount("#app");
